c66bd81fe71292155422a95d176a81cd009217eb
Kevin Noto <kevnoto@berkeley.edu>
2018-01-16 17:28:56 -0800

    Init Commit

---
+++ js/bookmarks.js
@@ -0,0 +1,49 @@
+var bookmarks = document.querySelector('.bookmarks-wrap');
+
+// loadBookmarks();
+
+function loadBookmarks() {
+    chrome.storage.sync.get({
+        bookmarks: []
+    }, function(items) {
+        if (items.bookmarks.length === 0) {
+            var bookmarksInfo = document.createElement('div');
+            bookmarksInfo.classList.add('bookmarks-info');
+            
+            var title = document.createElement('span');
+            title.textContent = 'No bookmarks yet.';
+            title.classList.add('title');
+
+            var subtitle = document.createElement('p');
+            subtitle.innerHTML = 'Add bookmarks on the <a href="options.html">options page</a>.';
+            subtitle.classList.add('subtitle');
+
+            bookmarksInfo.appendChild(title);
+            bookmarksInfo.appendChild(subtitle);
+
+            bookmarks.appendChild(bookmarksInfo);
+        } else {
+            items.bookmarks.forEach(function(bookmark) {
+                var newCategory = document.createElement('div');
+                newCategory.classList.add('category');
+
+                var categoryNameHtml = `<div class="category__name">${bookmark.category}</div>`;
+                var categoryLinksHtml = buildLinks(bookmark.links);
+
+                newCategory.innerHTML = Sanitizer.escapeHTML(categoryNameHtml + categoryLinksHtml);
+                bookmarks.appendChild(newCategory);
+            });
+        }
+    });
+}
+
+function buildLinks(links) {
+    var html = '<div class="category__links"><ul>';
+
+    links.forEach(function(link) {
+        html += `<a href="${link.url}"><li>${link.title}</li></a>`;
+    });
+
+    html += '</ul></div>';
+    return html;
+}
---
+++ js/weather.js
@@ -0,0 +1,49 @@
+(function() {
+
+    // Elements
+    var weather = document.querySelector('.weather');
+    var temperature = document.querySelector('.weather__temperature');
+    var unit = document.querySelector('.weather__unit');
+    var condition = document.querySelector('.weather__condition');
+    var conditionText = document.querySelector('.weather__condition-text');
+    var location = document.querySelector('.weather__location');
+
+	
+	// Weather API
+    var PATH_WEATHER_BASE = 'https://query.yahooapis.com/v1/public/yql';
+    var PATH_USER_LOCATION = '94702';
+    var PATH_USER_UNIT = 'f';
+    var PATH_SQL_QUERY = 'q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + PATH_USER_LOCATION + '") and u="' + PATH_USER_UNIT + '"';
+    var PATH_ARRAY_FORMAT = 'format=json&diagnostics=false&env=store://datatables.org/alltableswithkeys&callback=';
+
+    var url = encodeURI(PATH_WEATHER_BASE + '?' + PATH_SQL_QUERY + '&' + PATH_ARRAY_FORMAT);
+
+    // Query the API
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+            	weather.classList.add('active');
+                updateWeather(JSON.parse(request.responseText));
+                console.log(JSON.parse(request.responseText));
+            } else {
+                alert('Error getting weather information.');
+            }
+        }
+    }
+
+    request.open('GET', url);
+    request.send();
+
+    function updateWeather(data) {
+        data = data.query.results.channel;
+
+        temperature.innerHTML = data.item.condition.temp + '&deg;';
+        unit.innerHTML = data.units.temperature;
+        condition.classList.add('icon-' + data.item.condition.code);
+        conditionText.innerHTML = '';
+        // data.item.condition.text;
+        location.innerHTML = '';
+        // data.location.city + ', ' + data.location.country;
+    }
+})();
